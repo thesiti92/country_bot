003237f6d168b6900bdc7eafe75dc5ec70fc4849
Alexander Iansiti <aiansiti@college.harvard.edu>
2017-10-02 19:37:33 -0400

    Revert "new project"
    This reverts commit c6c60bb7135d65cfc10a8f8e9b3c47e7456c4be6.

--- facebook_mining.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-import json
-from datetime import datetime
-
-soup = BeautifulSoup(open("messages.html", encoding="utf8"), 'lxml').body.contents[1]
-
-json.dump(open("messages.json", 'w+'), [{'user': message.contents[0].get('div', class_="user").text, 'date': datetime(message.contents[0].get('div', class_="meta").text), 'message': message.next_sibling} for thread_set in soup.contents for thread in thread_set for message in thread.find_all('div') ])
